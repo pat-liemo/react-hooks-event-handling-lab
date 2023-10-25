531f63b89b9036c32cf504b243cb672fccf7cad0
patliemoaloo <aloemo77@gmail.com>
2023-10-25 21:00:25 +0300

    Complete assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    return (
+        <button onFocus={function() {
+            console.log("Good!");
+        }} onBlur={function() {
+            console.log("Hey! Eyes on me!")
+        }}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe
